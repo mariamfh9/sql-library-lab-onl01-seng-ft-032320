e869860257b352a912f8a996a7a21cdc8d768025
Mariam Haider <mfh.haider@gmail.com>
2020-04-20 21:27:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  SELECT title FROM books
+  "SELECT books.title AS book_titles 
+    FROM books 
+    INNER JOIN series 
+    ON books.id = series.id
     SELECT series.title AS title, COUNT(characters.series_id) AS num_species
       FROM series
       INNER JOIN characters
