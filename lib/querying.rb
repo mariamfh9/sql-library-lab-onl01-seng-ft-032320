efc0a12483484f0f28aea9ec9fb9c78da332888a
Mariam Haider <mfh.haider@gmail.com>
2020-04-20 22:19:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,7 +38,7 @@ def select_series_title_with_most_human_characters
         ON characters.id = character_books.character_id
       WHERE characters.species = 'human'
       GROUP BY series.title
-      ORDER BY characters.species DESC LIMIT 1
+      ORDER BY COUNT(characters.species) DESC LIMIT 1
       );"
 end
 
